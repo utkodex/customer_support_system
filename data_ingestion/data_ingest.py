9da04b6e96affa84b9e89881f8f8f7e7bdc1a72b
utkodex <bizzboosterdata@gmail.com>
2025-06-02 22:47:04 +0530

    updates data ingestions

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -4,12 +4,17 @@ import os
 import pandas as pd
 from data_ingestion.data_transform import data_converter
 
+load_dotenv()
 
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+ASTRA_DB_API_ENDPOINT=os.getenv("ASTRA_DB_API_ENDPOINT")
+ASTRA_DB_APPLICATION_TOKEN=os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+ASTRA_DB_KEYSPACE=os.getenv("ASTRA_DB_KEYSPACE")
 
 class ingest_data:
     
     def __init__(self):
-        print("data ingestion class has init")
+        print("data ingestion class has init...")
 
     def data_ingestion(self):
         pass
