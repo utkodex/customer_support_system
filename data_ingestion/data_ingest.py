602217cdcfe99f23af2ed561cfe259acd7119573
utkodex <bizzboosterdata@gmail.com>
2025-06-02 23:10:17 +0530

    updated data ingestions

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 from data_ingestion.data_transform import data_converter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 load_dotenv()
 
@@ -11,13 +12,36 @@ ASTRA_DB_API_ENDPOINT=os.getenv("ASTRA_DB_API_ENDPOINT")
 ASTRA_DB_APPLICATION_TOKEN=os.getenv("ASTRA_DB_APPLICATION_TOKEN")
 ASTRA_DB_KEYSPACE=os.getenv("ASTRA_DB_KEYSPACE")
 
+os.environ["GOOGLE_API_KEY"]=GOOGLE_API_KEY
+os.environ["ASTRA_DB_API_ENDPOINT"]=ASTRA_DB_API_ENDPOINT
+os.environ["ASTRA_DB_APPLICATION_TOKEN"]=ASTRA_DB_APPLICATION_TOKEN
+os.environ["ASTRA_DB_KEYSPACE"]=ASTRA_DB_KEYSPACE
+
 class ingest_data:
     
     def __init__(self):
         print("data ingestion class has init...")
+        self.embeddings = GoogleGenerativeAIEmbeddings(model="model/text-embedding-004")
+        self.data_converter=data_converter()
 
-    def data_ingestion(self):
-        pass
+    def data_ingestion(self, status):
+        vstore=AstraDBVectorStore(
+            embedding=self.embeddings,
+            collection_name="chatbotecomm",
+            api_endpoint=ASTRA_DB_API_ENDPOINT,
+            token=ASTRA_DB_APPLICATION_TOKEN,
+            namespace=ASTRA_DB_KEYSPACE,
+        )
+        storage=status
+
+        if storage==None:
+            docs=self.data_converter.data_transformation()
+            inserted_ids=vstore.add_documents(docs)
+            print(inserted_ids)
+        else:
+            return vstore
+        
+        return vstore, inserted_ids
 
 if __name__ == '__main__':
     data_ingestion = ingest_data()
