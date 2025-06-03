99d1c21484787d934e77d890879d7cb6996fd2a6
utkodex <bizzboosterdata@gmail.com>
2025-06-03 21:59:18 +0530

    Retriever code is updated

---
+++ main.py
@@ -0,0 +1,6 @@
+import uvicorn
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
---
+++ prompt_library/prompt.py
@@ -0,0 +1,3 @@
+PROMPT_TEMPATES={
+    
+}
