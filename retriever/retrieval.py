cb50342c6478bb436daba1ff4db7651f71a11eaa
utkodex <bizzboosterdata@gmail.com>
2025-06-03 22:22:09 +0530

    retrievel, css and html

--- retriever/retrieval.py
+++ retriever/retrieval.py
@@ -9,20 +9,54 @@ from dotenv import load_dotenv
 class Retriever:
 
     def __init__(self):
-        pass
+        self.model_loader=ModelLoader()
+        self.config=load_config()
+        self._load_env_variables()
+        self.vstore = None
+        self.retriever = None
 
     def _load_env_variables(self):
-        pass
 
-    def load_retriever(self):
-        pass
+        load_dotenv()
+         
+        required_vars = ["GOOGLE_API_KEY", "ASTRA_DB_API_ENDPOINT", "ASTRA_DB_APPLICATION_TOKEN", "ASTRA_DB_KEYSPACE"]
+        
+        missing_vars = [var for var in required_vars if os.getenv(var) is None]
+        
+        if missing_vars:
+            raise EnvironmentError(f"Missing environment variables: {missing_vars}")
+
+        self.google_api_key = os.getenv("GOOGLE_API_KEY")
+        self.db_api_endpoint = os.getenv("ASTRA_DB_API_ENDPOINT")
+        self.db_application_token = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+        self.db_keyspace = os.getenv("ASTRA_DB_KEYSPACE")
+
+    def create_retriever(self):
+        if not self.vstore:
+            collection_name = self.config["astra_db"]["collection_name"]
+            
+            self.vstore = AstraDBVectorStore(
+                embedding= self.model_loader.load_embeddings(),
+                collection_name=collection_name,
+                api_endpoint=self.db_api_endpoint,
+                token=self.db_application_token,
+                namespace=self.db_keyspace,
+            )
+        if not self.retriever:
+            top_k = self.config["retriever"]["top_k"] if "retriever" in self.config else 3
+            retriever = self.vstore.as_retriever(search_kwargs={"k": top_k})
+            print("Retriever loaded successfully.")
+            return retriever
+
 
     def call_retriever(self,query:str)-> List[Document]:
-        pass
+        retriever=self.create_retriever()
+        output=retriever.invoke(query)
+        return output
 
 if __name__=='__main__':
     retriever_obj = Retriever()
-    user_query = "Can you suggest good budget laptops?"
+    user_query = "Can you suggest good budget airpods?"
     results = retriever_obj.call_retriever(user_query)
 
     for idx, doc in enumerate(results, 1):
